96a77cc641aafaf6fbb0603e23a8dad63445d0bc
Enzo <enzo202p@gmail.com>
2022-07-25 14:31:40 -0300

    Versão final

--- src/componentes/Recall.js
+++ src/componentes/Recall.js
@@ -9,8 +9,7 @@ export default function Recall(){
                     {pergunta:'Podemos colocar __ dentro do JSX',resposta:'expressões'},
                     {pergunta:'O ReactDOM nos ajuda __', resposta:'interagindo com a DOM para colocar componentes React na mesma'},
                     {pergunta:'Usamos o npm para __', resposta:'gerenciar os pacotes necessários e suas dependências'},
-                    {pergunta:'Usamos props para __', resposta:'passar diferentes informações para componentes'},
-                    {pergunta:'Usamos estado (state) para __', resposta:'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'}];
+                    {pergunta:'Usamos props para __', resposta:'passar diferentes informações para componentes'}];
     const [respondidas, setRespondidas] = react.useState([]);
     const [contador, setContador] = react.useState(respondidas.length);
     return(
